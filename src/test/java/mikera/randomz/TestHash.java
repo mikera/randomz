9690707605cef46326bcb12a7255a6fd06725bef
mikera <mike.r.anderson.13@gmail.com>
2013-02-22 09:51:47 +0800

    Import zero-vector hashcode calculator

--- src/test/java/mikera/randomz/TestHash.java
+++ src/test/java/mikera/randomz/TestHash.java
@@ -1,6 +1,9 @@
 package mikera.randomz;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import mikera.randomz.Hash;
 
 import org.junit.Test;
@@ -11,5 +14,27 @@ public class TestHash {
 			double v=Math.sqrt(i);
 			assertEquals(new Double(v).hashCode(),Hash.hashCode(v));
 		}
+	}
+	
+	@Test
+	public void testDoubleHash() {
+		assertEquals(0,Hash.hashCode(0.0));
+		assertEquals(1,Hash.zeroVectorHash(0));
+		assertEquals(31,Hash.zeroVectorHash(1));
+		
+		for (int i=0; i<100; i++) {
+			ArrayList<Double> al=new ArrayList<Double>();
+			for (int j=0; j<i; j++) {
+				al.add(0.0);
+			}
+			
+			assertEquals(Hash.zeroVectorHash(i),al.hashCode());
+		}
+	}
+	
+	
+	@Test public void testZeroVectorHash() {
+		assertEquals(1,Hash.zeroVectorHash(0));
+		assertEquals(31,Hash.zeroVectorHash(1));
 	}
 }
